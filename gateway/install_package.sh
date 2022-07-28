2968696d82c161c7aa3736c72c7b94340c77e901
huyn29 <99779691+huyn29@users.noreply.github.com>
2022-07-28 13:37:51 +0700

    Update install_package.sh

--- gateway/install_package.sh
+++ gateway/install_package.sh
@@ -5,12 +5,12 @@ minikube addons enable ingress
 kubectl label namespace default istio-injection=enabled --overwrite
 
 # install istio
-helm repo add istio https://istio-release.storage.googleapis.com/charts
-helm repo update
+# helm repo add istio https://istio-release.storage.googleapis.com/charts
+# helm repo update
 
-kubectl create namespace istio-system
-helm upgrade istio-base istio/base -n istio-system --install
-helm upgrade istiod istio/istiod -n istio-system --wait --install
-#helm show values istio/gateway |nano -
-helm install istio-ingress istio/gateway -f make_gw.yaml -n istio-system --wait
-kubectl apply -f istio-ingressgateway.yaml
+# kubectl create namespace istio-system
+# helm upgrade istio-base istio/base -n istio-system --install
+# helm upgrade istiod istio/istiod -n istio-system --wait --install
+# #helm show values istio/gateway |nano -
+# helm install istio-ingress istio/gateway -f make_gw.yaml -n istio-system --wait
+# kubectl apply -f istio-ingressgateway.yaml
