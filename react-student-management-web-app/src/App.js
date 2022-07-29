f98e64e7d19d6c71a20024d37da9a3f585354e97
admin <nvh1892k@gmail.com>
2022-07-29 13:25:37 +0700

    done

--- react-student-management-web-app/src/App.js
+++ react-student-management-web-app/src/App.js
@@ -13,7 +13,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/students" className="navbar-brand">
-          Vietnam_HuyNguyen
+          Student App
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
